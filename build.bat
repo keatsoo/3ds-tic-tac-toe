4419f6aa92a617058f6993b2fd4d630f83007452
keatsoo <kitsunims@icloud.com>
2022-04-09 12:23:05 +0200

    fxed the script

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
+move /Y %~dp0\resources\icon.png %~dp0\
 make 
 PAUSE
-move /Y %~dp0/3ds-tic-tac-toe.3dsx %~dp0/output/
-move /Y %~dp0/3ds-tic-tac-toe.elf %~dp0/output/
-move /Y %~dp0/3ds-tic-tac-toe.smdh %~dp0/output/
+move /Y %~dp0\3ds-tic-tac-toe.3dsx %~dp0\output\
+move /Y %~dp0\3ds-tic-tac-toe.elf %~dp0\output\
+move /Y %~dp0\3ds-tic-tac-toe.smdh %~dp0\output\
+move /Y %~dp0\icon.png %~dp0\resources\
